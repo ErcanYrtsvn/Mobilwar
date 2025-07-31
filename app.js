afb88a726e4595cb721b9d95220a014edffc986b
Ercan <yurtsvn41@gmail.com>
2025-07-31 20:50:54 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function goTo(screenId) {
-  document.querySelectorAll('.screen').forEach(div => div.classList.add('hidden'));
-  document.querySelector('.menu').classList.add('hidden');
-  document.getElementById(screenId).classList.remove('hidden');
-}
-function goHome() {
-  document.querySelectorAll('.screen').forEach(div => div.classList.add('hidden'));
-  document.querySelector('.menu').classList.remove('hidden');
-}
